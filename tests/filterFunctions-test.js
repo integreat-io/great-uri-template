86669d77d857829f7710efa7f2337e33bad24dbb
Kjell-Morten <post@kjellmorten.no>
2019-08-30 12:09:40 +0200

    Update deps

--- tests/filterFunctions-test.js
+++ tests/filterFunctions-test.js
@@ -1,10 +1,10 @@
 import test from 'ava'
 
-import {compile, generate} from '..'
+import { compile, generate } from '..'
 
 test('should generate uri with filter functions', (t) => {
   const template = 'http://example.com/{section|append(_archive)}{?letter=name|max(1)|lower}'
-  const params = {section: 'news', name: 'John F.'}
+  const params = { section: 'news', name: 'John F.' }
   const expected = 'http://example.com/news_archive?letter=j'
 
   const compiled = compile(template)
@@ -15,7 +15,7 @@ test('should generate uri with filter functions', (t) => {
 
 test('should generate uri with escaped characters in filter args', (t) => {
   const template = 'http://example.com/{?keys=ids|wrap([, ", ", \\,\\{\\}])}'
-  const params = {ids: ['ent1', 'ent5']}
+  const params = { ids: ['ent1', 'ent5'] }
   const expected = 'http://example.com/?keys=%5B%22ent1%22%2C%22ent5%22%2C%7B%7D%5D'
 
   const compiled = compile(template)
--- tests/template-test.js
+++ tests/template-test.js
@@ -1,10 +1,10 @@
 import test from 'ava'
 
-import {compile, generate} from '..'
+import { compile, generate } from '..'
 
 test('should generate uri with replacement', (t) => {
   const template = 'http://example.com/{section}{?first,max}'
-  const params = {section: 'news', first: 40, max: 20}
+  const params = { section: 'news', first: 40, max: 20 }
   const expected = 'http://example.com/news?first=40&max=20'
 
   const compiled = compile(template)
@@ -15,7 +15,7 @@ test('should generate uri with replacement', (t) => {
 
 test('should generate uri with optional parameters', (t) => {
   const template = 'http://example.com/{section}{?first?,max?}'
-  const params = {section: 'news', max: 20}
+  const params = { section: 'news', max: 20 }
   const expected = 'http://example.com/news?max=20'
 
   const compiled = compile(template)
@@ -37,7 +37,7 @@ test('should generate empty string from empty string template', (t) => {
 
 test('should keep escaped curly brackets', (t) => {
   const template = '_design/store/_view/by_type_updatedAt?include_docs=true&startkey=["{type}"]&endkey=["{type}",\\{\\}]'
-  const params = {type: 'article'}
+  const params = { type: 'article' }
   const expected = '_design/store/_view/by_type_updatedAt?include_docs=true&startkey=%5B%22article%22%5D&endkey=%5B%22article%22,%7B%7D%5D'
 
   const compiled = compile(template)
@@ -48,7 +48,7 @@ test('should keep escaped curly brackets', (t) => {
 
 test('should escape reserved characters in params', (t) => {
   const template = 'http://example.com/{section}/{?url}'
-  const params = {section: 'health/wellness', url: 'http://test-it.com/'}
+  const params = { section: 'health/wellness', url: 'http://test-it.com/' }
   const expected = 'http://example.com/health%2Fwellness/?url=http%3A%2F%2Ftest-it.com%2F'
 
   const compiled = compile(template)
@@ -59,7 +59,7 @@ test('should escape reserved characters in params', (t) => {
 
 test('should not escape reserved characters in fragments', (t) => {
   const template = 'http://example.com/{#section}'
-  const params = {section: 'health/wellness'}
+  const params = { section: 'health/wellness' }
   const expected = 'http://example.com/#health/wellness'
 
   const compiled = compile(template)
